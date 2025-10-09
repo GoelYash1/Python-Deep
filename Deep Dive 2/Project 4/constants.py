2f63af0a72ae32ec0001105a2ce0187baa71e94e
GoelYash1 <ygoel172001@gmail.com>
2025-10-09 17:16:02 +0530

    context managers and project 4

---
+++ Deep Dive 2/Project 4/constants.py
@@ -0,0 +1,26 @@
+import os
+from parse_utils import parse_date
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Consistent and cross-platform path construction
+fname_personal = os.path.join(current_dir, "data", "personal_info.csv")
+fname_employment = os.path.join(current_dir, "data", "employment.csv")
+fname_vehicles = os.path.join(current_dir, "data", "vehicles.csv")
+fname_update_status = os.path.join(current_dir, "data", "update_status.csv")
+
+fnames = (fname_personal, fname_employment, fname_vehicles, fname_update_status)
+
+# Parsers
+personal_parser = (str, str, str, str, str)
+employment_parser = (str, str, str, str)
+vehicle_parser = (str, str, str, int)
+update_status_parser = (str, parse_date, parse_date)
+parsers = personal_parser, employment_parser, vehicle_parser, update_status_parser
+
+# Named Tuple Names
+personal_class_name = 'Personal'
+employment_class_name = 'Employment'
+vehicle_class_name = 'Vehicle'
+update_status_class_name = 'UpdateStatus'
+class_names = personal_class_name, employment_class_name, vehicle_class_name, update_status_class_name
---
+++ Deep Dive 2/Project 4/playground.py
@@ -0,0 +1,5 @@
+from parse_utils import iter_file
+from constants import fnames, classnames, parsers
+from itertools import islice
+
+
