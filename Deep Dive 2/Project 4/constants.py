c80183b5cf21bfd4f79f98671086b6da68bf3bf3
GoelYash1 <ygoel172001@gmail.com>
2025-10-10 12:53:17 +0530

    Project 4

--- Deep Dive 2/Project 4/constants.py
+++ Deep Dive 2/Project 4/constants.py
@@ -9,7 +9,7 @@ fname_employment = os.path.join(current_dir, "data", "employment.csv")
 fname_vehicles = os.path.join(current_dir, "data", "vehicles.csv")
 fname_update_status = os.path.join(current_dir, "data", "update_status.csv")
 
-fnames = (fname_personal, fname_employment, fname_vehicles, fname_update_status)
+fnames = fname_personal, fname_employment, fname_vehicles, fname_update_status
 
 # Parsers
 personal_parser = (str, str, str, str, str)
@@ -24,3 +24,12 @@ employment_class_name = 'Employment'
 vehicle_class_name = 'Vehicle'
 update_status_class_name = 'UpdateStatus'
 class_names = personal_class_name, employment_class_name, vehicle_class_name, update_status_class_name
+
+# Field Inclusion/Exclusion
+personal_fields_compress = [True, True, True, True, True]
+employment_fields_compress = [True, True, True, False]
+vehicle_fields_compress = [False, True, True, True]
+update_status_fields_compress = [False, True, True]
+compress_fields = (personal_fields_compress, employment_fields_compress,
+                   vehicle_fields_compress, update_status_fields_compress)
+
